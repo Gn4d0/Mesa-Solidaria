68ad38bbe313300935f694a844e93df6e42733ae
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-22 07:37:52 -0300

    Delete estrutura De código Header em local errado

--- mesa_solidaria/src/App.jsx
+++ mesa_solidaria/src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './componentes/Header/header'
+
 
 
 function App() {
 
   return (
     <>
-       <Header/>
-
-       <Footer/>
+       
     </>
   )
 }
