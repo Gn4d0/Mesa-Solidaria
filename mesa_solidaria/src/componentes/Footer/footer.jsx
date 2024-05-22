e9d648506bf8551ea87db4cdfdacdc25a8edade9
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-22 11:37:22 -0300

    Criação do código estrutural do component footer

---
+++ mesa_solidaria/src/componentes/Footer/footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer class={Style.footer}>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Cadastro</a>
+        <a href="">Carrinho</a>
+        <a href="">Produtos</a>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
--- mesa_solidaria/src/componentes/Header/header.jsx
+++ mesa_solidaria/src/componentes/Header/header.jsx
@@ -1,22 +1,17 @@
-import styles from './Header.css'
+import styles from "./Header.css";
 
 function Header() {
-    return (
-  
-        <header className = {styles.header}>
-  
-            <span> Mesa Solidária </span>
-            <nav> 
-                <a href= "">Home</a>
-                <a href= "">Cadastro</a>
-                <a href= "">Produtos</a>
-                <a href= "">Carrinho</a>
-            </nav>
-  
-  
-        </header>
+  return (
+    <header className={styles.header}>
+      <span> Mesa Solidária </span>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Cadastro</a>
+        <a href="">Produtos</a>
+        <a href="">Carrinho</a>
+      </nav>
+    </header>
+  );
+}
 
-    )
-  }
-  
-  export default Header
+export default Header;
