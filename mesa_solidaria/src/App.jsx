c4c2757b1924d338c6bdba0548388666ade580af
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-23 02:47:27 -0300

    Estrutura Funcional de Rotas

--- mesa_solidaria/src/App.jsx
+++ mesa_solidaria/src/App.jsx
@@ -1,6 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+
 function App() {
   return (
     <div className="App">
+      <h1>React Router</h1>
+      <p>NavBar</p>
+      <Outlet />
+      <p>Footer</p>
       
     </div>
   );
--- mesa_solidaria/src/main.jsx
+++ mesa_solidaria/src/main.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from './routes/Home/home.jsx';
-import Cadastro from './routes/Cadastro/cadastro.jsx';
-import Carrinho from './routes/Carrinho/carrinho.jsx';
-import Produtos from './routes/Produtos/produtos.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home/home.jsx";
+import Cadastro from "./routes/Cadastro/Cadastro.jsx";
+import Carrinho from "./routes/Carrinho/carrinho.jsx";
+import Produtos from "./routes/Produtos/produtos.jsx";
+import App from "./App.jsx";
 
 const router = createBrowserRouter([
   {
-    path:"/", element: <Home />
+    path:"/", element: <Home />,
   },
   {
-    path:"cadastro", element: <Cadastro />
+    path:"cadastro", element: <Cadastro />,
   },
   {
-    path:"carrinho", element: <Carrinho />
+    path:"carrinho", element: <Carrinho />,
   },
   {
-    path:"produtos", element: <Produtos />
+    path:"produtos", element: <Produtos />,
   }
 
 ]);
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
