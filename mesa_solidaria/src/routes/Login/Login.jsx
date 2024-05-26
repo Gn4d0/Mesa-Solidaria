a644f2c3bcc4f38d8c4b3f417e500cf093c583c7
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-26 04:31:13 -0300

    Organização da estrutura

--- mesa_solidaria/src/routes/Login/Login.jsx
+++ mesa_solidaria/src/routes/Login/Login.jsx
@@ -12,40 +12,40 @@ const Login = () => {
         <div className={"circunferencia"}>
           <img src="\src\assets\img\image.png"></img>
         </div>
-      </div>
+        <div className="conteudo">
+          <form>
+            <h1>Login</h1>
 
-      <div className="conteudo">
-        <form>
-          <h1>Login</h1>
-          
-          <div>
-            <input type="email" placeholder="E-mail ou CNPJ" />
-            <FaUser className="icon" />
-          </div>
-          
-          <div>
-            <input type="password" placeholder="Password" />
-            <FaLock className="icon" />
-          </div>
+            <div>
+              <input type="email" placeholder="E-mail ou CNPJ" />
+              <FaUser className="icon" />
+            </div>
 
-          <div className={"recall"}></div>
+            <div>
+              <input type="password" placeholder="Password" />
+              <FaLock className="icon" />
+            </div>
+
+            <div className={"recall"}></div>
             <label>
               <input type="checkbox " />
               Lembre de mim
-
             </label>
-            <a href = "#">Esqueceu a senha?</a>
+            <a href="#">Esqueceu a senha?</a>
 
-          
+            <button>Entrar</button>
 
-          <button>Entrar</button>
-
-          <div className="signup-link">
-            <p>Não tem uma conta? <a href="#">Registrar</a></p>
-          </div>
-          <p>Ao continuar, você concorda em receber comunicações com ofertas. Confira nossa Declaraçao de Privacidade 
-</p>
-        </form>
+            <div className="signup-link">
+              <p>
+                Não tem uma conta? <a href="#">Registrar</a>
+              </p>
+            </div>
+            <p>
+              Ao continuar, você concorda em receber comunicações com ofertas.
+              Confira nossa Declaraçao de Privacidade
+            </p>
+          </form>
+        </div>
       </div>
 
       <Footer />
