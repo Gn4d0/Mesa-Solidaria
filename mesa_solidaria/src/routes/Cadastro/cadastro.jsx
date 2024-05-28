89dd0f5f7cb3646cd09bdce6e5b031627d524c8b
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-28 10:43:03 -0300

    Inclusão de img

--- mesa_solidaria/src/routes/Cadastro/cadastro.jsx
+++ mesa_solidaria/src/routes/Cadastro/cadastro.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { useState } from "react";
+import Header from "../../componentes/Header/header";
+import Footer from "../../componentes/Footer/footer";
 
 const Cadastro = () => {
+  const [tipoUsuario, setTipoUsuario] = useState("fisico");
+  const handleChangeTipoUsuario = (event) => {
+    setTipoUsuario(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Página do Cadastro</h1>
-    </div>
+    <>
+      <Header />
+      <div>
+        <h2>Sua Empresa Pede</h2>
+        <p>
+          Participe da Mesa Solídária Reduza o Desperdício Ofereça produtos e
+          apoie pessoas em vulnerabilidade social. Cadastre sua empresa.
+        </p>
+        <p>Comece agora a revolução do seu negócio</p>
+        <div>
+          <h2>Cadastre-se</h2>
+          <form>
+            <div>
+              <input
+                name="tipoUsuario"
+                id="fisico"
+                type="radio"
+                value="fisico"
+                checked={tipoUsuario === "fisico"}
+                onChange={handleChangeTipoUsuario}
+              />
+              <label class="form-check-label fw-bold" for="fisico">Pessoa Física</label>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer/>
+    </>
   );
 };
 
