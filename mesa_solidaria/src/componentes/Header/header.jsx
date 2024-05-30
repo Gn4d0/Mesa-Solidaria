15cf77bb2f45590fb54c48bfc3864a955c85fd41
Agnaldo Silva <gnadocursos@gmail.com>
2024-05-30 14:12:16 -0300

    Reorganização da estruturação da page Home

--- mesa_solidaria/src/componentes/Header/header.jsx
+++ mesa_solidaria/src/componentes/Header/header.jsx
@@ -2,31 +2,35 @@ import "/src/componentes/Header/cabe.css";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
+import logo from "/src/assets/img/logo.png";
+
 const Header = () => {
   return (
-    <nav className={"navbar"}>
-      <container>
-        <nav className={"navbar"}></nav>
-        <Link to="/">
-          <img src="\src\assets\img\logo.png"></img>
-        </Link>
-        <ul className={"list"}>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cadastro">Cadastro</NavLink>
-          </li>
-          <li>
-            <NavLink to="/produtos">Produtos</NavLink>
-          </li>
-        </ul>
-      </container>
+    <header>
       <div>
+        <Link to="/" className="logotipoENome">
+          <img src={logo} className="logotipo" />
+          <h1 className="nomeProjeto">Mesa Solidária</h1>
+        </Link>
+        <nav className="navbar">
+          <ul className="list">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cadastro">Cadastro</NavLink>
+            </li>
+            <li>
+              <NavLink to="/produtos">Produtos</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="botoes">
         <button>Criar Conta</button>
         <button>Entrar</button>
       </div>
-    </nav>
+    </header>
   );
 };
 
